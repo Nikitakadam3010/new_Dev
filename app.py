abd54029b2abe13061c7b686b85d244044e99da9
Nikitakadam3010 <135589025+Nikitakadam3010@users.noreply.github.com>
2024-05-15 11:51:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": "null",
+   "execution_count": None,
    "id": "5099355e-94ab-420e-aba3-33c0a123392f",
    "metadata": {},
    "outputs": [],
